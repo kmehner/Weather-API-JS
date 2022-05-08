80aebdd3e09f655f73106091f4da3cc72d009e78
Katelyn Mehner <kmehner@gmail.com>
2022-05-08 13:34:26 -0600

    Housekeeping and styling

--- js/main.js
+++ js/main.js
@@ -10,52 +10,48 @@
         e.preventDefault();
 
         // Get location 
-        let location = e.target.location.value
-        console.log(location)
+        let cityZip = e.target.cityZip.value;
+        console.log(cityZip);
 
         // fetch weather info
-        let locationInfo = await getWeather(location)
-        console.log(locationInfo)
-
-        // create header for cards?
+        let locationInfo = await getWeather(cityZip);
+        console.log(locationInfo);
 
         // create card for info we want (at minimum - high, low, forcast, humidity)
-        let weatherInfoCards = await weatherCards(locationInfo)
-        console.log("Printing weather cards")
-
+        let weatherInfoCards = await weatherCards(locationInfo);
     }
-
-    // function to fetch api 
-    // viewing in the terminal 
-        // res = fetch('https://api.openweathermap.org/data/2.5/weather?q=london&units=imperial&appid={apiKey}').then(res => res.json()).then(data => console.log(data))
     
-    async function getWeather(location){
-        console.log("Fetching the API")
-        let apiKey = ""; // Avoid uploading API key to github
-        let apiKey2 = ""
+    async function getWeather(cityZip){
+        console.log("Fetching the API");
+        // in apiKeys to avoid uploading maintain privacy
+        let apiKey = "";
+        let apiKey2 = ""; 
         try{
    
-            console.log('Fetching zip')
-
-            let res2 = await fetch (`https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${location}&countryFilter=US&apiKey=${apiKey2}`)
-            console.log(res2)
-
+            // Fetches the location by name or zip code (better error handling and allows inclusion of zip code)
+            console.log('Fetching the location')
+            let res2 = await fetch (`https://api.myptv.com/geocoding/v1/locations/by-text?searchText=${cityZip}&countryFilter=US&apiKey=${apiKey}`);
             let data2 = await res2.json()
             console.log(data2)
 
-            // Would use this to find info by longitude and latitude - different reference long&lat in dbs so searching by name
+            // Would use this to find info by longitude and latitude - different reference long&lat in apis so searching by name
             // let lat = data2['locations'][0]['referencePosition']['latitude'].toFixed(4)
             // let long = data2['locations'][0]['referencePosition']['longitude'].toFixed(4)
             // console.log (lat, long, "lat and long")
+            // let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`)
 
+            // Specify city for fetching the weather data
             let city = data2['locations'][0]['address']['city']
+
+            // Assign state to global variable for the Weather Info Card
             state = data2['locations'][0]['address']['state']
             console.log (city, "city")
 
-            // let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`)
-            let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+            console.log("Fetching the weather data")
+            let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey2}&units=imperial`)
             let data = await res.json()
 
+            // Adjust background dependent on weather status (Eventually want to make day/night theme)
             if (data['weather'][0]['main'] == 'snow'){ 
                 document.body.style.backgroundImage ="url('/static/images/snowy.jpg')"
             }else if (data['clouds']['all'] < 50){
@@ -71,9 +67,8 @@
         }
     };
 
-     // create card for info we want (at minimum - high, low, forcast, humidity)
+     // create card for weather info
     async function weatherCards(locationInfo){
-        console.log("Making the card") 
 
         // Grab card display
         const cardDiv = document.getElementById("weatherDisplay")
@@ -81,7 +76,6 @@
 
         // accessing the data 
         let name = locationInfo['name']
-        let country = locationInfo['sys']['country']
         let currentWeather = locationInfo['main']['temp']
         let weatherCondition = locationInfo['weather'][0]['main']
         let feelsLike = locationInfo['main']['feels_like']
@@ -91,7 +85,6 @@
         let tempMin = locationInfo['main']['temp_min']
         let iconID = locationInfo['weather'][0]['icon']
         let iconUrl = `http://openweathermap.org/img/wn/${iconID}@2x.png`
-
 
         // Card Div
         const card = document.createElement('div')
@@ -103,58 +96,43 @@
 
         // Add icon to top of card 
         const cardImg = document.createElement('img')
-        cardImg.className = 'card-img-top'
+        cardImg.className = 'card-img card-img-top'
         cardImg.src = iconUrl
-        card.append(cardImg)
 
         // Card title
         const cardTitle = document.createElement('h3')
         cardTitle.className = 'card-text'
         cardTitle.innerHTML = `${name} | ${state}`
-        card.append(cardTitle)
 
-        // Card Text
+        // --- Card Text ---
         const cardCurrent = document.createElement('p')
         cardCurrent.className = 'card-text'
         cardCurrent.innerHTML = `Currently: ${currentWeather}\u00B0 F and ${weatherCondition}`
-        card.append(cardCurrent)
-
-        // find forecast
-        console.log(weatherCondition)
 
         const cardFeelsLike = document.createElement('p')
         cardFeelsLike.className = 'card-text'
         cardFeelsLike.innerHTML = `Feels like: ${feelsLike}\u00B0 F`
-        card.append(cardFeelsLike)
 
         const cardMax = document.createElement('p')
         cardMax.className = 'card-text'
         cardMax.innerHTML = `High: ${tempMax}\u00B0 F`
-        card.append(cardMax)
 
         const cardMin = document.createElement('p')
         cardMin.className = 'card-text'
         cardMin.innerHTML = `Low: ${tempMin}\u00B0 F`
-        card.append(cardMin)
 
         const cardHumidity = document.createElement('p')
         cardHumidity.className = 'card-text'
         cardHumidity.innerHTML = `Humidity: ${humidity}%`
-        card.append(cardHumidity)
 
         const cardForecast = document.createElement('p')
         cardForecast.className = 'card-text'
         cardForecast.innerHTML = `Chance of Precipitation: ${clouds}%`
-        card.append(cardForecast)
 
-        // Add card body to card and add card to cardDiv
+        // Append text to card, card to card body, and assign card to cardDiv in index.html
+        card.append(cardImg, cardTitle, cardCurrent, cardFeelsLike, cardMax, cardMin, cardHumidity, cardForecast)
         card.append(cardBody)
         cardDiv.append(card)
-        
     }
-
-
-
     form.addEventListener('submit', handleSubmit);
-
 }
